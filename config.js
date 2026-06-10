5483baad85f1e3a5c7a33fa7c929a86138b2540c
Fabio <fabioconefe@gmail.com>
2026-06-10 18:50:18 +0200

    fix(config): force single header row for periodico sheet to prevent Gviz auto-header merging

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ const CONFIG = {
     players:        "https://docs.google.com/spreadsheets/d/1Qp1kD61ofzpqIlbUErOX3Bi1NISGJknix6im8mn0wOo/gviz/tq?tqx=out:csv&sheet=players",
     special_events: "https://docs.google.com/spreadsheets/d/1Qp1kD61ofzpqIlbUErOX3Bi1NISGJknix6im8mn0wOo/gviz/tq?tqx=out:csv&sheet=special_events",
     predictions:    "https://docs.google.com/spreadsheets/d/1Qp1kD61ofzpqIlbUErOX3Bi1NISGJknix6im8mn0wOo/gviz/tq?tqx=out:csv&sheet=Respuestas de formulario 1",
-    periodico:      "https://docs.google.com/spreadsheets/d/1Qp1kD61ofzpqIlbUErOX3Bi1NISGJknix6im8mn0wOo/gviz/tq?tqx=out:csv&sheet=periodico"
+    periodico:      "https://docs.google.com/spreadsheets/d/1Qp1kD61ofzpqIlbUErOX3Bi1NISGJknix6im8mn0wOo/gviz/tq?tqx=out:csv&sheet=periodico&headers=1"
   },
 
   googleForm: {
